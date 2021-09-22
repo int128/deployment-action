2cf83f1c637bd59a192950d4d377356b6ec73b82
Hidetake Iwata <int128@gmail.com>
2021-09-22 15:31:26 +0900

    Ignore error in previous deletion (#8)

--- src/run.ts
+++ src/run.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+import { RequestError } from '@octokit/request-error'
 import { DeploymentInputs, inferDeploymentParameters } from './deployment'
 
 type Inputs = {
@@ -12,23 +13,29 @@ export const run = async (inputs: Inputs): Promise<void> => {
   })
   const p = inferDeploymentParameters(github.context, inputs)
 
-  core.info(`Finding existing deployments for environment ${p.environment}`)
-  const existing = await octokit.rest.repos.listDeployments({
+  core.info(`Finding previous deployments for environment ${p.environment}`)
+  const previous = await octokit.rest.repos.listDeployments({
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
     environment: p.environment,
   })
-  core.info(`Deleting ${existing.data.length} deployment(s)`)
-  await Promise.all(
-    existing.data.map(async (deployment) => {
+  core.info(`Deleting previous ${previous.data.length} deployment(s)`)
+  for (const deployment of previous.data) {
+    try {
       await octokit.rest.repos.deleteDeployment({
         owner: github.context.repo.owner,
         repo: github.context.repo.repo,
         deployment_id: deployment.id,
       })
       core.info(`Deleted deployment ${deployment.url}`)
-    })
-  )
+    } catch (error) {
+      if (error instanceof RequestError && error.status === 422) {
+        core.warning(`unable to delete previous deployment ${deployment.url}: ${error.message}`)
+        continue
+      }
+      throw error
+    }
+  }
 
   core.info(`Creating deployment for environment=${p.environment}, ref=${p.ref}, sha=${p.sha}`)
   const created = await octokit.rest.repos.createDeployment({
