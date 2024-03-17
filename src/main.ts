d735f078d27fd524fbc24ea6b731e86eb4335c97
Hidetake Iwata <int128@gmail.com>
2024-03-17 16:44:51 +0900

    Refactor (#713)

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,17 @@ import * as core from '@actions/core'
 import { run } from './run'
 
 const main = async (): Promise<void> => {
-  await run({
+  const outputs = await run({
     environment: core.getInput('environment') || undefined,
     environmentSuffix: core.getInput('environment-suffix') || undefined,
     description: core.getInput('description') || undefined,
     task: core.getInput('task') || undefined,
     token: core.getInput('token', { required: true }),
   })
+  core.info(`Setting outputs: ${JSON.stringify(outputs, undefined, 2)}`)
+  core.setOutput('url', outputs.url)
+  core.setOutput('id', outputs.id)
+  core.setOutput('node-id', outputs.nodeId)
 }
 
 main().catch((e: Error) => {
