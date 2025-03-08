f9a66796b0a62d574ae2215b24b383904d2ffb43
Hidetake Iwata <int128@gmail.com>
2025-03-08 20:41:52 +0900

    Refactor deployment functions (#1014)
    * Refactor
    
    * Refactor
    
    * Refactor
    
    * Refactor

--- src/deployment.ts
+++ src/deployment.ts
@@ -1,26 +1,16 @@
+import assert from 'assert'
+import * as core from '@actions/core'
 import * as github from './github.js'
+import { Octokit, RestEndpointMethodTypes } from '@octokit/action'
 
-export type DeploymentInputs = {
-  environment?: string
-  environmentSuffix?: string
-}
-
-export type DeploymentParameters = {
+export type DeploymentContext = {
   ref: string
   sha: string
   environment: string
   transient_environment?: boolean
 }
 
-export const inferDeploymentParameters = (context: github.Context, inputs: DeploymentInputs): DeploymentParameters => {
-  const p = infer(context)
-  return {
-    ...p,
-    environment: `${inputs.environment ?? p.environment}${inputs.environmentSuffix ?? ''}`,
-  }
-}
-
-const infer = (context: github.Context): DeploymentParameters => {
+export const inferDeploymentFromContext = (context: github.Context): DeploymentContext => {
   if ('pull_request' in context.payload) {
     return {
       // set the head ref to associate a deployment with the pull request
@@ -45,3 +35,82 @@ const infer = (context: github.Context): DeploymentParameters => {
     environment: `${context.workflow}/${context.eventName}`,
   }
 }
+
+type DeleteExistingDeployments = {
+  owner: string
+  repo: string
+  environment: string
+}
+
+export const deleteExistingDeployments = async (req: DeleteExistingDeployments, octokit: Octokit) => {
+  core.info(`Finding the deployments of ${req.environment}`)
+  const deployments = await octokit.rest.repos.listDeployments({
+    owner: req.owner,
+    repo: req.repo,
+    environment: req.environment,
+  })
+
+  core.info(`Deleting ${deployments.data.length} deployments`)
+  for (const deployment of deployments.data) {
+    try {
+      await octokit.rest.repos.deleteDeployment({
+        owner: req.owner,
+        repo: req.repo,
+        deployment_id: deployment.id,
+      })
+      core.info(`Deleted the deployment: ${deployment.url}`)
+    } catch (error) {
+      if (isRequestError(error)) {
+        core.warning(`Unable to delete the deployment: ${deployment.url}: ${error.status} ${error.message}`)
+        continue
+      }
+      throw error
+    }
+  }
+}
+
+type CreateDeployment = {
+  owner: string
+  repo: string
+  ref: string
+  sha: string
+  environment: string
+  transient_environment?: boolean
+  description?: string
+  task?: string
+  state: RestEndpointMethodTypes['repos']['createDeploymentStatus']['parameters']['state']
+}
+
+export const createDeployment = async (req: CreateDeployment, octokit: Octokit) => {
+  core.info(`Creating a deployment: ${JSON.stringify(req, undefined, 2)}`)
+  const created = await octokit.rest.repos.createDeployment({
+    owner: req.owner,
+    repo: req.repo,
+    ref: req.ref,
+    sha: req.sha,
+    environment: req.environment,
+    transient_environment: req.transient_environment,
+    description: req.description,
+    task: req.task,
+    auto_merge: false,
+    required_contexts: [],
+  })
+  assert.strictEqual(created.status, 201)
+  core.info(`Created a deployment: ${created.data.url}`)
+
+  core.info(`Setting the deployment status to ${req.state}`)
+  const { data: deploymentStatus } = await octokit.rest.repos.createDeploymentStatus({
+    owner: req.owner,
+    repo: req.repo,
+    deployment_id: created.data.id,
+    state: req.state,
+  })
+  core.info(`Created a deployment status: ${deploymentStatus.url}`)
+
+  return created.data
+}
+
+type RequestError = Error & { status: number }
+
+const isRequestError = (error: unknown): error is RequestError =>
+  error instanceof Error && 'status' in error && typeof error.status === 'number'
--- src/run.ts
+++ src/run.ts
@@ -1,13 +1,13 @@
-import assert from 'assert'
-import * as core from '@actions/core'
 import * as github from './github.js'
 import { Octokit } from '@octokit/action'
-import { DeploymentInputs, inferDeploymentParameters } from './deployment.js'
+import { createDeployment, deleteExistingDeployments, inferDeploymentFromContext } from './deployment.js'
 
 type Inputs = {
+  environment?: string
+  environmentSuffix?: string
   description?: string
   task?: string
-} & DeploymentInputs
+}
 
 type Outputs = {
   url: string
@@ -16,44 +16,18 @@ type Outputs = {
 }
 
 export const run = async (inputs: Inputs, octokit: Octokit, context: github.Context): Promise<Outputs> => {
-  const params = inferDeploymentParameters(context, inputs)
+  const deployment = inferDeploymentFromContext(context)
 
-  core.info(`Finding the previous deployments of the environment ${params.environment}`)
-  const previous = await octokit.rest.repos.listDeployments({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    environment: params.environment,
-  })
-  core.info(`Found ${previous.data.length} deployment(s)`)
-  for (const deployment of previous.data) {
-    try {
-      await octokit.rest.repos.deleteDeployment({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        deployment_id: deployment.id,
-      })
-      core.info(`Deleted the previous deployment ${deployment.url}`)
-    } catch (error) {
-      if (isRequestError(error)) {
-        core.warning(`Unable to delete the previous deployment ${deployment.url}: ${error.status} ${error.message}`)
-        continue
-      }
-      throw error
-    }
-  }
+  const environment = `${inputs.environment ?? deployment.environment}${inputs.environmentSuffix ?? ''}`
 
-  core.info(`Creating a deployment of the environment ${params.environment} at ref=${params.ref}, sha=${params.sha}`)
-  const created = await octokit.rest.repos.createDeployment({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    auto_merge: false,
-    required_contexts: [],
-    description: inputs.description,
-    task: inputs.task,
-    ...params,
-  })
-  assert(created.status === 201)
-  core.info(`Created a deployment ${created.data.url}`)
+  await deleteExistingDeployments(
+    {
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      environment: environment,
+    },
+    octokit,
+  )
 
   // If the deployment is not deployed for a while, GitHub will show the below error.
   //   This branch had an error being deployed
@@ -61,23 +35,25 @@ export const run = async (inputs: Inputs, octokit: Octokit, context: github.Cont
   //
   // To avoid this, set the deployment status to inactive immediately.
   const initialState = 'inactive'
-  core.info(`Setting the deployment status to ${initialState}`)
-  await octokit.rest.repos.createDeploymentStatus({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    deployment_id: created.data.id,
-    state: initialState,
-  })
-  core.info(`Set the deployment status to ${initialState}`)
+
+  const created = await createDeployment(
+    {
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      environment: environment,
+      ref: deployment.ref,
+      sha: deployment.sha,
+      transient_environment: deployment.transient_environment,
+      description: inputs.description,
+      task: inputs.task,
+      state: initialState,
+    },
+    octokit,
+  )
 
   return {
-    url: created.data.url,
-    id: created.data.id,
-    nodeId: created.data.node_id,
+    url: created.url,
+    id: created.id,
+    nodeId: created.node_id,
   }
 }
-
-type RequestError = Error & { status: number }
-
-const isRequestError = (error: unknown): error is RequestError =>
-  error instanceof Error && 'status' in error && typeof error.status === 'number'
